f41920d987947161ea23e36b10f71069e593b0b3
Yanchuk Andrey <orpiy@mail.ru>
2024-11-30 12:02:36 +0700

    update exported-functions.js

--- exported-functions.js
+++ exported-functions.js
@@ -12,3 +12,6 @@ export const normalize = (object) => {
     object.description = object.description.toLowerCase();
     return object;
 };
+/*-----------------------------------------------------*/
+export const is = (obj1, obj2) => obj1.name === obj2.name && obj1.state === obj2.state && obj1.website === obj2.website;
+/*-----------------------------------------------------*/
