9e6ef8c104e4ad182dadc75415e8bdb9e3f342b8
Yanchuk Andrey <orpiy@mail.ru>
2024-12-02 04:55:17 +0700

    update exported-functions.js

--- exported-functions.js
+++ exported-functions.js
@@ -3,32 +3,44 @@ import _ from "lodash";
 // import example from "./example.json" assert { type: "json" };
 /*-----------------------------------------------------*/
 export const getExampleJson = (object) => {
-    return object;
+  return object;
 };
 /*-----------------------------------------------------*/
 export const normalize = (object) => {
-    if (Object.keys(object).length === 0) return "Specify a non-empty object!";
-    object.name = _.capitalize(object.name);
-    object.description = object.description.toLowerCase();
-    return object;
+  if (Object.keys(object).length === 0) return "Specify a non-empty object!";
+  object.name = _.capitalize(object.name);
+  object.description = object.description.toLowerCase();
+  return object;
 };
 /*-----------------------------------------------------*/
-export const is = (obj1, obj2) => obj1.name === obj2.name && obj1.state === obj2.state && obj1.website === obj2.website;
+export const is = (obj1, obj2) =>
+  obj1.name === obj2.name &&
+  obj1.state === obj2.state &&
+  obj1.website === obj2.website;
 /*-----------------------------------------------------*/
-
 export const getDomainInfo = (link) => {
-    let scheme = 'http';
-    let name = link;
-    if (link.startsWith('https://')) {
-        scheme = 'https';
-        name = link.replace('https://', '');
-    }
-    if (link.startsWith('http://')) {
-        scheme = 'http';
-        name = link.replace('http://', '');
-    }
-    return {
-        scheme,
-        name
-    };
+  let scheme = "http";
+  let name = link;
+  if (link.startsWith("https://")) {
+    scheme = "https";
+    name = link.replace("https://", "");
+  }
+  if (link.startsWith("http://")) {
+    scheme = "http";
+    name = link.replace("http://", "");
+  }
+  return {
+    scheme,
+    name,
+  };
 };
+/*-----------------------------------------------------*/
+export const countWords = (sentence) => {
+  if (!sentence) return {};
+  const result = {};
+  for (const word of _.words(sentence.toLowerCase())) {
+    result[word] = (result[word] ?? 0) + 1;
+  }
+  return result;
+};
+/*-----------------------------------------------------*/
